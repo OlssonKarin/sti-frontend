da5d17b173ff1599f7f47aa8a0e881fcb785f3bb
Karin Olsson <karin_m_olsson@hotmail.com>
2022-02-15 16:10:34 +0100

    fixing issues

--- public/js/index.js
+++ public/js/index.js
@@ -1,154 +1,151 @@
-/* const { set } = require("express/lib/application"); */
+const { set } = require("express/lib/application");
 
-// selecting canvas
-const canvas = document.querySelector("canvas");
-canvas.width = innerWidth;
-canvas.heigt = innerHeight
+// Selecting Canvas
+const canvas = document.querySelector('canvas')
+canvas.width = innerWidth
+canvas.height = innerHeight
 
-window.addEventListener("resize", () => {
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
-    stopGame();
+window.addEventListener('resize', () => {
+  canvas.width = innerWidth
+  canvas.height = innerHeight
+  stopGame()
 })
 
-// variable & constants
+// Variables & Constants
+const c = canvas.getContext('2d')
+const scoreEl = document.getElementById('scoreEl')
+const highestEl = document.getElementById('highestEl')
+const startGameBtn = document.getElementById('startGameBtn')
+const modelEl = document.getElementById('modelEl')
+const bigScoreEl = document.getElementById('bigScoreEl')
+const friction = 0.98
+let x = canvas.width / 2
+let y = canvas.height / 2
+let projectiles = []
+let enemies = []
+let particles = []
+let score = 0
+let highest = localStorage.getItem('highest') || 0
+let animationId
+let spanEnemiesInterval
+let spawnTime = 1000
+highestEl.innerHTML = highest
 
-const c = canvas.getContext("2D");
-const scoreEl = document.getElementById("scoreEl");
-const highestEl = document.getElementById("highestEl");
-const startGamebtn = document.getElementById("statGameBtn")
-const modelEl = document.getElementById("medelEl");
-const bigScoreEl = document.getElementById("bigScoreEl");
-const friction = 0.98;
-let x = canvas.width /2;
-let y = canvas.heigt /2;
-let projectiles = [];
-let enemies = [];
-let particles = [];
-let score = 0;
-let highest = localStorage.getItem("highest") || 0;
-let animatedId;
-let spanEnemiesInterval;
-let spawnTime = 1000;
-highestEl.innerHTML = highest;
-
-//Starting ball Class
-
+// Starting Ball Class
 class Ball {
-    constructor(x, y, radius, color){
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.color = color;
-    }
+  constructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
 
-    draw() {
-        c.beginPath();
-        c.arc(this.x, this.y, this.radius, Math.PI * 2, 0, false)
-        c.fillStyle = this.color;
-        c.fill();
-    }
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, Math.PI * 2, 0, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
 }
 
-// shooter ball for movin ball
+// Shooter Ball for Moving Ball
 class Shooter extends Ball {
-    constructor(x, y, radius, color, velocity){
-        super(x, y, radius, color);
-        this.velocity = velocity; 
-    }
-    
-    update() {
-        this.draw()
-        this.x = this.x + this.velocity.x
-        this.y = this.y + this.velocity.y
-    }
+  constructor(x, y, radius, color, velocity) {
+    super(x, y, radius, color)
+    this.velocity = velocity
+  }
+
+  update() {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
 }
 
-// Particle for exploding Shooter ball
+// Particle for Exploding Shooter BAll
 class Particle extends Shooter {
-    constructor(x, y, radius, color, velocity){
-        super(x, y, radius, color, velocity)
-        this.alpha = 1
-    }
-    draw() {
-        c.save()
-        c.globalAlpha = this.alpha
-        c.beginPath()
-        c.arc(this.x, this.y, this.radius, Math.PI * 2, 0, false)
-        c.fillStyle = this.color
-        c.fill()
-        c.restore()
-    }
+  constructor(x, y, radius, color, velocity) {
+    super(x, y, radius, color, velocity)
+    this.alpha = 1
+  }
 
-    update() {
-        this.draw()
-        this.velocity.x *= friction
-        this.velocity.y *= friction
-        this.x = this.x + this.velocity.x * 2
-        this.y = this.y + this.velocity.y * 2
-        this.alpha -= 0.01
-    }
+  draw() {
+    c.save()
+    c.globalAlpha = this.alpha
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, Math.PI * 2, 0, false)
+    c.fillStyle = this.color
+    c.fill()
+    c.restore()
+  }
+
+  update() {
+    this.draw()
+    this.velocity.x *= friction
+    this.velocity.y *= friction
+    this.x = this.x + this.velocity.x * 2
+    this.y = this.y + this.velocity.y * 2
+    this.alpha -= 0.01
+  }
 }
 
 function updateScore(times = 1) {
-    spawnTime *= 0.9995
-    score += 100 * times
-    scoreEl.innerHTML = score
+  spawnTime *= 0.9995
+  score += 100 * times
+  scoreEl.innerHTML = score
 }
 
-// calculate velocity from center (x, y) to (x1, y1)
-
+// Calculate Velocity from center(x, y) to (x1,y1)
 function calculateVelocity(
-    x,
-    y,
-    x1 = canvas.width / 2,
-    y1 = canvas.height / 2
-  ) {
-    const angle = Math.atan2(y1 - y, x1 - x)
-    const velocity = {
-      x: Math.cos(angle),
-      y: Math.sin(angle),
-    }
-  
-    return velocity
+  x,
+  y,
+  x1 = canvas.width / 2,
+  y1 = canvas.height / 2
+) {
+  const angle = Math.atan2(y1 - y, x1 - x)
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
   }
 
-  //Animation
-  function animate() {
-      animationId = requestAnimationFrame(animate);
-      c.fillStyle = "rgba(0,0,0,0.1)";
-      c.fillRect(0,0, canvas.width, canvas.height);
-      player.draw();
-  }
+  return velocity
+}
+
+// Animation
+function animate() {
+  animationId = requestAnimationFrame(animate)
+  c.fillStyle = 'rgba(0,0,0,0.1)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  player.draw()
 
   // Updates and remove particles
   particles.forEach((particle, index) => {
-      if (particle.alpha <= 0) {
-      set.Timeout(() => {
-          particles.splice(index, 1)
-      }, 0)    
-      } else {
-          particle.update()
-      }
+    if (particle.alpha <= 0) {
+      setTimeout(() => {
+        particles.splice(index, 1)
+      }, 0)
+    } else {
+      particle.update()
+    }
   })
 
-  //Update and remove projectiles
-  particles.forEach((particle, index) => {
-      projectile.update()
+  // Update and remove projectiles
+  projectiles.forEach((projectile, index) => {
+    projectile.update()
     if (
-        projectile.x + projectile.radius < 1 ||
-        projectile.x - projectile.radius > canvas.width ||
-        projectile.y + projectile.radius < 0 ||
-        projectile.y - projectile.radius > canvas.height
-      ){
-        set.Timeout(() => {
-            projectiles.splice(index, 1)
-        }, 0)    
-    }    
-})
+      projectile.x + projectile.radius < 1 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      setTimeout(() => {
+        projectiles.splice(index, 1)
+      }, 0)
+    }
+  })
 
-// Update & destroy enemies, create explosions & increase score
-enemies.forEach((enemy, index) => {
+  // Update & Destroy Enemies, Create Explosions & Increase Score
+  enemies.forEach((enemy, index) => {
     enemy.update()
 
     // Calculate distance between player(player.x, player.y) and enemy(enemy.x, enemy.y) using Math.hypot(perpendicular, base) which gives hypotenuse / distance between them
@@ -197,84 +194,76 @@ enemies.forEach((enemy, index) => {
       }
     })
   })
-
+}
 
 // Shoot Enemy
 function shootEnemy(e) {
-    let x = canvas.width / 2,
+  let x = canvas.width / 2,
     y = canvas.height / 2
 
-    v = calculateVelocity(x, y, e.clientX, e.clientY)
-    v.x *= 5.5
-    v.y *= 5.5
+  v = calculateVelocity(x, y, e.clientX, e.clientY)
+  v.x *= 5.5
+  v.y *= 5.5
 
-    projectiles.push(new Shooter(x, y, 5, "white", v))
+  projectiles.push(new Shooter(x, y, 5, 'white', v))
 }
 
-// Reinitializing variables for starting a new game
+// Reinitializing Variables for Starting a New Game
 function init() {
-    player = new Ball(x, y, 10, "white")
-    projectiles = []
-    enemies = []
-    particles = []
-    score = 0
-    spawnTime = 1000
-    highestEl.innerHTML = score
-    scoreEl.innerHTML = score
-    highestEl.innerHTML = highest    
+  player = new Ball(x, y, 10, 'white')
+  projectiles = []
+  enemies = []
+  particles = []
+  score = 0
+  spawnTime = 1000
+  highestEl.innerHTML = score
+  scoreEl.innerHTML = score
+  highestEl.innerHTML = highest
 }
 
-// Stop game
-
+// Stop Game
 function stopGame() {
-    function stopGame() {
-        clearInterval(spanEnemiesInterval)
-        cancelAnimationFrame(animationId) // Exit Animation
-        canvas.removeEventListener('click', shootEnemy) // Stop Shooting
-        modelEl.style.display = 'flex' // Dialogue box
-        if (score > highest) {
-          highest = score
-          localStorage.setItem('highest', highest)
-        }
-        bigScoreEl.innerHTML = score // Poping score
-      }
+  clearInterval(spanEnemiesInterval)
+  cancelAnimationFrame(animationId) // Exit Animation
+  canvas.removeEventListener('click', shootEnemy) // Stop Shooting
+  modelEl.style.display = 'flex' // Dialogue box
+  if (score > highest) {
+    highest = score
+    localStorage.setItem('highest', highest)
+  }
+  bigScoreEl.innerHTML = score // Poping score
 }
 
-// spawning random enemies
-
+// Spawning Random Enemies
 function spanEnemies() {
-    // spawn enemy every second
-    spanEnemiesInterval = setTimeout(() => {
-        let x, y
-        const radius = Math.random() * 16 + 14
-        if (Math.random() < 0.5) {
-          x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
-          y = Math.random() * canvas.height
-        } else {
-          x = Math.random() * canvas.width
-          y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
-        }
-        const color = `hsl(${Math.floor(Math.random() * 360)}, 50%, 50%)`
-        enemies.push(new Shooter(x, y, radius, color, calculateVelocity(x, y)))
-        spanEnemies()
-      }, spawnTime)
-   
-}
-
-//start new game
-function startGame() {
-    x = canvas.width / 2
-    y = canvas.height / 2
-    canvas.addEventListener("click", shootEnemy)
-    init()
-    animate()
-    clearInterval(spanEnemiesInterval)
+  // Spawn a enemy every second
+  spanEnemiesInterval = setTimeout(() => {
+    let x, y
+    const radius = Math.random() * 16 + 14
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+      y = Math.random() * canvas.height
+    } else {
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+    }
+    const color = `hsl(${Math.floor(Math.random() * 360)}, 50%, 50%)`
+    enemies.push(new Shooter(x, y, radius, color, calculateVelocity(x, y)))
     spanEnemies()
-    modelEl.style.display = "none";
+  }, spawnTime)
 }
 
-// start game button
-startGamebtn.addEventListener("click", startGame); 
+// Start New Game
+function startGame() {
+  x = canvas.width / 2
+  y = canvas.height / 2
+  canvas.addEventListener('click', shootEnemy)
+  init()
+  animate()
+  clearInterval(spanEnemiesInterval)
+  spanEnemies()
+  modelEl.style.display = 'none'
+}
 
-
-
+// Start Game Button
+startGameBtn.addEventListener('click', startGame)
