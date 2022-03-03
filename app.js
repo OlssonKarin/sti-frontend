10b7285757c97b3803979557895cfa573e41ef90
Karin Olsson <karin_m_olsson@hotmail.com>
2022-03-03 11:18:51 +0100

    fixed connections

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use('healthcheck', require('./routes/healthcheck.routes'));
 app.use(express.static('public'))
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + "/public/index.html")
+    res.sendFile(__dirname + "/public/tetris.html")
 
 })
 
